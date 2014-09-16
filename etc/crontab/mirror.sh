f45df575c2672984f30fe3014f0fcf7c0b8bbbdf
Michael Aïvázis <michael.aivazis@orthologue.com>
2014-09-16 02:11:30 -0700

    etc: added support for automatic mirroring of the repository

---
+++ etc/crontab/mirror.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# michael a.g. aivazis
+# orthologue
+# (c) 1998-2014 all rights reserved
+#
+
+# bzr
+bzr="/usr/bin/bzr"
+
+# pull config
+echo pulling
+${bzr} pull
+echo done
+
+# end of file
