c974ead69b83c49a9bfd748959c00da4435b5602
Michael Aivazis <michael.aivazis@orthologue.com>
2015-02-06 10:00:39 -0800

    web: added the {bower} steps to the {live} target

--- web/Make.mm
+++ web/Make.mm
@@ -1,7 +1,7 @@
 # -*- Makefile -*-
 #
 # Alec Aivazis <alec@aivazis.com>
-# 
+#
 # (c) 2014-2015 all rights reserved
 #
 
@@ -9,10 +9,14 @@
 include feynman.def
 # the package
 PACKAGE=web/www
+
+# the target folders
+EXPORT_RESOURCES = resources
+EXPORT_TEMPLATES = templates
 # the package
-EXPORT_WEB = \
-    resources \
-    templates
+EXPORT_WEB = $(EXPORT_RESOURCES) $(EXPORT_TEMPLATES)
+# dependency management
+DEP_INSTALL = /usr/local/bin/bower install
 
 # standard targets
 all: export
@@ -20,5 +24,8 @@ all: export
 export:: export-web
 
 live: live-web
+	$(SSH) $(PROJ_LIVE_USERURL) \
+           '$(CD) $(PROJ_LIVE_PCKGDIR)/$(PROJECT)/$(EXPORT_RESOURCES); $(DEP_INSTALL)'
+
 
 # end of file
